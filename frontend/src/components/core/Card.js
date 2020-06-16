a0c4e773ecb1c1857264d8696bd995d4ecf04616
trthanhson253 <trthanhson253@gmail.com>
2020-06-16 00:54:26 -0400

    6-16-20

--- frontend/src/components/core/Card.js
+++ frontend/src/components/core/Card.js
@@ -52,163 +52,171 @@ const Card = ({ company, loading }) => {
     loadAverageRating(slug);
   }, []);
 
-  return !loading ? (
-    <Spinner />
-  ) : (
-    <div data-href="/companies/vht" className="company-item">
-      <div className="company-info">
-        <figure className="company-info__logo image is-64x64">
-          <img
-            src={`${API}/company/photo/${company.slug}`}
-            alt={company.name}
-            style={{ maxHeight: '100%', maxWidth: '100%' }}
-          />
-        </figure>
-        <div className="company-info__detail">
-          <h1 className="is-size-5 has-text-weight-semibold company-info__name">
-            <Link to={`/companies/${company.slug}`}>{company.name}</Link>{' '}
-            <span className="company-info__rating">
-              {company.avgRating >= 1 ? (
-                <span>
-                  <span className="icon is-small has-text-warning">
-                    <i
-                      className={
-                        company.avgRating >= 1 ? 'fas fa-star' : 'far fa-star'
-                      }
-                    />
-                  </span>
-                  <span className="icon is-small has-text-warning">
-                    <i
-                      className={
-                        company.avgRating >= 2
-                          ? 'fas fa-star'
-                          : company.avgRating >= 1.5
-                          ? 'fas fa-star-half-alt'
-                          : 'far fa-star'
-                      }
-                    />
-                  </span>
-                  <span className="icon is-small has-text-warning">
-                    <i
-                      className={
-                        company.avgRating >= 3
-                          ? 'fas fa-star'
-                          : company.avgRating >= 2.5
-                          ? 'fas fa-star-half-alt'
-                          : 'far fa-star'
-                      }
-                    />
-                  </span>
-                  <span className="icon is-small has-text-warning">
-                    <i
-                      className={
-                        company.avgRating >= 4
-                          ? 'fas fa-star'
-                          : company.avgRating >= 3.5
-                          ? 'fas fa-star-half-alt'
-                          : 'far fa-star'
-                      }
-                    />
-                  </span>
-                  <span className="icon is-small has-text-warning">
-                    <i
-                      className={
-                        company.avgRating == 5
-                          ? 'fas fa-star'
-                          : company.avgRating >= 4.5
-                          ? 'fas fa-star-half-alt'
-                          : 'far fa-star'
-                      }
-                    />
-                  </span>
+  return (
+    <div>
+      {!loading ? (
+        <Spinner />
+      ) : (
+        <div data-href="/companies/vht" className="company-item">
+          <div className="company-info">
+            <figure className="company-info__logo image is-64x64">
+              <img
+                src={`${API}/company/photo/${company.slug}`}
+                alt={company.name}
+                style={{ maxHeight: '100%', maxWidth: '100%' }}
+              />
+            </figure>
+            <div className="company-info__detail">
+              <h1 className="is-size-5 has-text-weight-semibold company-info__name">
+                <Link to={`/companies/${company.slug}`}>{company.name}</Link>{' '}
+                <span className="company-info__rating">
+                  {company.avgRating >= 1 ? (
+                    <span>
+                      <span className="icon is-small has-text-warning">
+                        <i
+                          className={
+                            company.avgRating >= 1
+                              ? 'fas fa-star'
+                              : 'far fa-star'
+                          }
+                        />
+                      </span>
+                      <span className="icon is-small has-text-warning">
+                        <i
+                          className={
+                            company.avgRating >= 2
+                              ? 'fas fa-star'
+                              : company.avgRating >= 1.5
+                              ? 'fas fa-star-half-alt'
+                              : 'far fa-star'
+                          }
+                        />
+                      </span>
+                      <span className="icon is-small has-text-warning">
+                        <i
+                          className={
+                            company.avgRating >= 3
+                              ? 'fas fa-star'
+                              : company.avgRating >= 2.5
+                              ? 'fas fa-star-half-alt'
+                              : 'far fa-star'
+                          }
+                        />
+                      </span>
+                      <span className="icon is-small has-text-warning">
+                        <i
+                          className={
+                            company.avgRating >= 4
+                              ? 'fas fa-star'
+                              : company.avgRating >= 3.5
+                              ? 'fas fa-star-half-alt'
+                              : 'far fa-star'
+                          }
+                        />
+                      </span>
+                      <span className="icon is-small has-text-warning">
+                        <i
+                          className={
+                            company.avgRating == 5
+                              ? 'fas fa-star'
+                              : company.avgRating >= 4.5
+                              ? 'fas fa-star-half-alt'
+                              : 'far fa-star'
+                          }
+                        />
+                      </span>
+                    </span>
+                  ) : (
+                    <span>
+                      <span className="icon is-small has-text-warning">
+                        <i className="far fa-star" />
+                      </span>
+                      <span className="icon is-small has-text-warning">
+                        <i className="far fa-star" />
+                      </span>
+                      <span className="icon is-small has-text-warning">
+                        <i className="far fa-star" />
+                      </span>
+                      <span className="icon is-small has-text-warning">
+                        <i className="far fa-star" />
+                      </span>
+                      <span className="icon is-small has-text-warning">
+                        <i className="far fa-star" />
+                      </span>
+                    </span>
+                  )}
                 </span>
-              ) : (
-                <span>
-                  <span className="icon is-small has-text-warning">
-                    <i className="far fa-star" />
-                  </span>
-                  <span className="icon is-small has-text-warning">
-                    <i className="far fa-star" />
-                  </span>
-                  <span className="icon is-small has-text-warning">
-                    <i className="far fa-star" />
-                  </span>
-                  <span className="icon is-small has-text-warning">
-                    <i className="far fa-star" />
-                  </span>
-                  <span className="icon is-small has-text-warning">
-                    <i className="far fa-star" />
-                  </span>
-                </span>
-              )}
-            </span>
-          </h1>
+              </h1>
 
-          <div className="company-info__other">
-            <span
-              style={{
-                marginRight: '0.3rem',
-                color: '#FF3860',
-              }}
-            >
-              <span className="icon">
-                {' '}
-                <i className="fas fa-briefcase" />
-              </span>{' '}
-              {company.type == 0 ? (
-                <td>Product</td>
-              ) : company.type > 1 ? (
-                <td>Consultancy</td>
-              ) : (
-                <td>Service</td>
-              )}
-            </span>
-            -
-            <span>
-              <span className="icon">
-                {' '}
-                <i className="fas fa-building" />{' '}
-              </span>{' '}
-              {company.city} , {company.state}
-            </span>
+              <div className="company-info__other">
+                <span
+                  style={{
+                    marginRight: '0.3rem',
+                    color: '#FF3860',
+                  }}
+                >
+                  <span className="icon">
+                    {' '}
+                    <i className="fas fa-briefcase" />
+                  </span>{' '}
+                  {company.type == 0 ? (
+                    <td>Product</td>
+                  ) : company.type > 1 ? (
+                    <td>Consultancy</td>
+                  ) : (
+                    <td>Service</td>
+                  )}
+                </span>
+                -
+                <span>
+                  <span className="icon">
+                    {' '}
+                    <i className="fas fa-building" />{' '}
+                  </span>{' '}
+                  {company.city} , {company.state}
+                </span>
+              </div>
+            </div>
           </div>
+
+          <div
+            className="level-right"
+            style={{
+              paddingRight: '10px',
+              fontSize: 'small',
+              color: 'gray',
+              fontWeight: 'bold',
+            }}
+          >
+            <b></b>
+            <span>
+              [Posted {moment(company.createdAt).format('MM-DD-YYYY')}]
+            </span>
+            &nbsp;| &nbsp;
+            <i class="fas fa-comment-alt"></i>&nbsp; {comments.length} Comment
+            {comments.length > 1 ? 's' : ''}&nbsp;| &nbsp;
+            <i class="fas fa-share"></i>&nbsp;+Share| &nbsp;
+            {!isAlreadyLoved(company._id) && (
+              <span onClick={() => handleLove(company.slug)}>
+                <i class="far fa-heart"></i>&nbsp;{love1} Love| &nbsp;
+              </span>
+            )}
+            {isAlreadyLoved(company._id) && (
+              <span style={{ color: '#BD3734' }}>
+                <i class="fas fa-heart"></i>&nbsp;{love1} Loved| &nbsp;
+              </span>
+            )}
+            <i class="fas fa-eye"></i>&nbsp; {company.view} View
+            {company.view > 1 ? 's' : ''}| &nbsp;
+            <Link to={`/companies/${company.slug}`}>
+              <i class="fas fa-hand-point-right"></i>See
+            </Link>
+          </div>
+
+          <br />
+          <hr />
         </div>
-      </div>
-
-      <div
-        className="level-right"
-        style={{
-          paddingRight: '10px',
-          fontSize: 'small',
-          color: 'gray',
-          fontWeight: 'bold',
-        }}
-      >
-        <b></b>
-        <span>[Posted {moment(company.createdAt).format('MM-DD-YYYY')}]</span>
-        &nbsp;| &nbsp;
-        <i class="fas fa-comment-alt"></i>&nbsp; {comments.length} Comment
-        {comments.length > 1 ? 's' : ''}&nbsp;| &nbsp;
-        <i class="fas fa-share"></i>&nbsp;+Share| &nbsp;
-        {!isAlreadyLoved(company._id) && (
-          <span onClick={() => handleLove(company.slug)}>
-            <i class="far fa-heart"></i>&nbsp;{love1} Love| &nbsp;
-          </span>
-        )}
-        {isAlreadyLoved(company._id) && (
-          <span style={{ color: '#BD3734' }}>
-            <i class="fas fa-heart"></i>&nbsp;{love1} Loved| &nbsp;
-          </span>
-        )}
-        <i class="fas fa-eye"></i>&nbsp; {company.view} View
-        {company.view > 1 ? 's' : ''}| &nbsp;
-        <Link to={`/companies/${company.slug}`}>
-          <i class="fas fa-hand-point-right"></i>See
-        </Link>
-      </div>
-
-      <br />
-      <hr />
+      )}
     </div>
   );
 };
